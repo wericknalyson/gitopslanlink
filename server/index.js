b2a0fc5855464db817fa3be4ba603fa9ba6945a0
Werick Nalyson <werick-nalyson@mac.local>
2024-02-06 17:51:42 -0300

    test

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Version 1.0.0</h1>')
+    res.send('<h1>Version 1.0.1</h1>')
 })
 
 app.listen(3000, () => {
